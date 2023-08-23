a88b8f116ace3e762e3e186d8d71b215d00702be
BETHA HEMANTH <bethahemanth7264@gmail.com>
2023-08-23 23:42:55 +0530

    be aware

---
+++ Arrays/src/Arrays/hello.java
@@ -0,0 +1,9 @@
+package Arrays.src.Arrays;
+
+public class hello {
+    public static void main(String args[])
+    {
+        System.out.println("Be aware of abhi he will steal your ip addresses");
+    }
+
+}
